2fe5ae31fb7c74b503ce244ba90be6601e6d5ccd
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-14 11:56:20 -0600

    Agregando estructura del controlador

--- ApplicacionWebp/Controllers/TablaController.cs
+++ ApplicacionWebp/Controllers/TablaController.cs
@@ -8,10 +8,17 @@ namespace ApplicacionWebp.Controllers
 {
     public class TablaController : Controller
     {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido_Paterno { get; set; }
+        public string Apellido_Materno { get; set; }
+        public string Edad { get; set; }
+        public bool isactive { get; set; }
+
         // GET: Tabla
         public ActionResult Index()
         {
-
 
             return View();
         }
