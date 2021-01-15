8b96c540bd32c96439db8ba7f28c0ecbdfb4a38b
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-15 13:51:16 -0600

    Agregando Funcionalidad de botones finalizado

--- ApplicacionWebp/Scripts/Modal.js
+++ ApplicacionWebp/Scripts/Modal.js
@@ -1,24 +1,31 @@
 ﻿
 
-const btnEditar = document.querySelector('#editar');
+const btnEditar = document.querySelectorAll('#editar');
 const btnAgregar = document.querySelector('#agregar');
 const btnEditarAregar = document.querySelector('#editarAgregar');
 const btnAgregarCampo = document.querySelector('#agregarCampo');
+const modalH2 = document.querySelector('#modalCrearEditar h2');
+
 
 eventListeners();
 function eventListeners() {
 
-    btnEditar.addEventListener('click', mostrarBtn);
+    btnEditar.forEach(btnedit => {
+        btnedit.addEventListener('click', mostrarBtnEditar);
+    });
     btnAgregar.addEventListener('click', mostrarBtnAgregar);
+
 }
 
-function mostrarBtn() {
+function mostrarBtnEditar() {
     btnAgregarCampo.style.display = 'none';
     btnEditarAregar.style.display = 'inline-block';
+    modalH2.textContent = 'Editar Usuario';
+
 }
 
 function mostrarBtnAgregar() {
     btnAgregarCampo.style.display = 'inline-block';
     btnEditarAregar.style.display = 'none';
-
+    modalH2.textContent = 'Crear Usuario';
 }
