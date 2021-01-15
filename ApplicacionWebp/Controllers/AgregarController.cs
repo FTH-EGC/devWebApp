e9267c6e0665dcec96fe21bd8dbdfdcd4ee95d4d
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-15 10:04:44 -0600

    Creacion de la tabla principal - iniciando modal

---
+++ ApplicacionWebp/Controllers/AgregarController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApplicacionWebp.Models.ViewModels;
+using ApplicacionWebp.Models;
+
+namespace ApplicacionWebp.Controllers
+{
+    public class AgregarController : Controller
+    {
+        // GET: Agregar
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (usuariosEntities db = new usuariosEntities())
+            {
+                lst = (from d in db.tbl_datos
+                       select new ListTablaViewModel
+                       {
+                           Id = d.id,
+                           Nombre = d.nombre,
+                           Apellido_Paterno = d.apellido_paterno,
+                           Apellido_Materno = d.apellido_materno,
+                           Edad = d.edad,
+                           isactive = d.isactve
+                       }).ToList();
+
+            }
+
+                return View(lst);
+        }
+    }
+}
