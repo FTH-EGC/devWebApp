12794f32df677eaf4ccb0fbe6a2a1dfcb8ae0b2f
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-15 14:33:06 -0600

    Agregando validacion

--- ApplicacionWebp/Scripts/Modal.js
+++ ApplicacionWebp/Scripts/Modal.js
@@ -5,6 +5,13 @@ const btnAgregar = document.querySelector('#agregar');
 const btnEditarAregar = document.querySelector('#editarAgregar');
 const btnAgregarCampo = document.querySelector('#agregarCampo');
 const modalH2 = document.querySelector('#modalCrearEditar h2');
+const modal = document.querySelector('#modalCrearEditar');
+const modalForm = document.querySelector('#modalCrearEditar form');
+const nombre = document.querySelector('#nombreInput');
+const apellidoP = document.querySelector('#apellidoPInput');
+const apellidoM = document.querySelector('#apellidoMInput');
+const edad = document.querySelector('#edadInput');
+const select = document.querySelector('#select');
 
 
 eventListeners();
@@ -14,7 +21,8 @@ function eventListeners() {
         btnedit.addEventListener('click', mostrarBtnEditar);
     });
     btnAgregar.addEventListener('click', mostrarBtnAgregar);
-
+    btnEditarAregar.addEventListener('click', validar);
+    btnAgregarCampo.addEventListener('click', validar);
 }
 
 function mostrarBtnEditar() {
@@ -28,4 +36,30 @@ function mostrarBtnAgregar() {
     btnAgregarCampo.style.display = 'inline-block';
     btnEditarAregar.style.display = 'none';
     modalH2.textContent = 'Crear Usuario';
+}
+
+
+function validar() {
+    if (nombre.value === "" || apellidoP.value === "" || apellidoM.value === "" || edad.value === "" || select.value === "") {
+        const error = document.querySelector('.error');
+        if (!error) {
+            mostrarError();
+        }
+        return;
+    }
+}
+
+function mostrarError() {
+    const div = document.createElement('div');
+    const parrafo = document.createElement('p');
+    parrafo.textContent = 'Todos los campos son obligatorios';
+    parrafo.classList.add('text-danger', 'h4');
+    div.appendChild(parrafo);
+    div.classList.add('text-center', 'error');
+    modalForm.insertBefore(div, modalForm.children[0]);
+
+    const error = document.querySelector('.error');
+    setTimeout(() => {
+        error.remove();
+    }, 3000)
 }
