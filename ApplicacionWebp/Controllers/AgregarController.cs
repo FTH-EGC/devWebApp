54a83781d31dbb9518ff03e6fa1a249d9acaccb1
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-18 10:11:51 -0600

    Agregando funcionalidad del modal, trayendo datos con Jquery

--- ApplicacionWebp/Controllers/AgregarController.cs
+++ ApplicacionWebp/Controllers/AgregarController.cs
@@ -11,23 +11,30 @@ namespace ApplicacionWebp.Controllers
     public class AgregarController : Controller
     {
         // GET: Agregar
+        usuariosEntities db = new usuariosEntities();
         public ActionResult Index()
         {
+
+            return View();
+        }
+
+
+        public JsonResult GetData()
+        {
             List<ListTablaViewModel> lst;
-            using (usuariosEntities db = new usuariosEntities())
-            {
-                lst = (from d in db.tbl_datos
-                       select new ListTablaViewModel
-                       {
-                           Id = d.id,
-                           Nombre = d.nombre,
-                           Apellido_Paterno = d.apellido_paterno,
-                           Apellido_Materno = d.apellido_materno,
-                           Edad = d.edad,
-                           isactive = d.isactve
-                       }).ToList();
-            }
-            return View(lst);
+            lst = (from d in db.tbl_datos
+                   select new ListTablaViewModel
+                   {
+                       Id = d.id,
+                       Nombre = d.nombre,
+                       Apellido_Paterno = d.apellido_paterno,
+                       Apellido_Materno = d.apellido_materno,
+                       Edad = d.edad,
+                       isactive = d.isactve
+                   }).ToList();
+
+
+            return Json(lst, JsonRequestBehavior.AllowGet);
         }
     }
 }
