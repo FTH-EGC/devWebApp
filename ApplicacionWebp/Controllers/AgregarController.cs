d6a87f73ef48ebcb7179fa52ed18f69208b999d2
Erick Garcia <ironmaiden-emylle@hotmail.com>
2021-01-20 13:30:05 -0600

    Trayendo info de la BD a los inputs

--- ApplicacionWebp/Controllers/AgregarController.cs
+++ ApplicacionWebp/Controllers/AgregarController.cs
@@ -34,47 +34,13 @@ namespace ApplicacionWebp.Controllers
         }
 
 
-        public JsonResult GetUsuarioById(int UserId)
+        public ActionResult GetUsuarioById(int UserId)
         {
-            tbl_datos model = db.tbl_datos.Where(d => d.id == UserId).SingleOrDefault();
-            string value = string.Empty;
+            DataAccessLayout objDB = new DataAccessLayout();
 
-            value = JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings
-            {
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            }); 
-
-            return Json(value, JsonRequestBehavior.AllowGet);
+            return Json(objDB.GetDataBYID(UserId), JsonRequestBehavior.AllowGet);
 
         }
 
-
-        public JsonResult SaveDatainDB(ListTablaViewModel model)
-        {
-
-            var result = false;
-            try
-            {
-
-                if (model.Id > 0)
-                {
-                    tbl_datos usu = db.tbl_datos.SingleOrDefault(d => d.id == model.Id);
-                    usu.nombre = model.Nombre;
-                    usu.apellido_paterno = model.Apellido_Paterno;
-                    usu.apellido_materno = model.Apellido_Materno;
-                    usu.edad = model.Edad;
-                    usu.isactve = model.isactive;
-                    db.SaveChanges();
-                    result = true;
-                }
-
-            }
-            catch (Exception ex) {
-                throw ex;
-            }
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-
-        }
     }
 }
--- ApplicacionWebp/DataAccess/DataAccessLayout.cs
+++ ApplicacionWebp/DataAccess/DataAccessLayout.cs
@@ -61,5 +61,54 @@ namespace ApplicacionWebp.DataAccess
 
 
         }
+        public ListTablaViewModel GetDataBYID(int idUs)
+        {
+            SqlConnection con = null;
+            DataSet ds = null;
+            ListTablaViewModel usuObj = null;
+
+            try
+            {
+
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+
+                SqlCommand cmd = new SqlCommand("sp_Admin", con);
+                con.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Action", "SELECTID");
+                cmd.Parameters.AddWithValue("@UserId", idUs);
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                ds = new DataSet();
+                da.Fill(ds);
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    usuObj = new ListTablaViewModel();
+                    usuObj.Id = Convert.ToInt32(ds.Tables[0].Rows[i]["id"].ToString());
+                    usuObj.Nombre = ds.Tables[0].Rows[i]["nombre"].ToString();
+                    usuObj.Apellido_Paterno = ds.Tables[0].Rows[i]["apellido_paterno"].ToString();
+                    usuObj.Apellido_Materno = ds.Tables[0].Rows[i]["apellido_materno"].ToString();
+                    usuObj.Edad = ds.Tables[0].Rows[i]["edad"].ToString();
+                    usuObj.isactive = Convert.ToBoolean(ds.Tables[0].Rows[i]["isactve"]);
+                }
+
+                return usuObj;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+        }
+
+
     }
 }
